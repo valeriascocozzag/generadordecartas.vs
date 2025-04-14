5e32b7af7654bb7379ff34a7747b8988c305f5ca
Valeria Scocozza <valeriascocozzag@gmail.com>
2025-04-14 19:53:50 +0000

    generador de cartas: valeria scocozza

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  generateCard();
+  document.getElementById("generateBtn").addEventListener("click", generateCard);
 };
+
+function generateCard() {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let suits = ["Hearts", "Diamonds", "Spades", "Clubs"];
+  let symbols = {
+   Hearts: "♥",
+   Diamonds: "♦",
+   Spades: "♠",
+   Clubs: "♣"
+  };
+
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+  let randomSymbol = symbols[randomSuit];
+
+  document.getElementById("cardContent").innerHTML = randomNumber;
+  document.querySelector(".top").innerHTML = randomSymbol;
+  document.querySelector(".bottom").innerHTML = randomSymbol;
+
+  let card = document.getElementById("theCard");
+  card.className = "card";
+  card.classList.add("suit-" + randomSuit.toLowerCase());
+}
+
+
